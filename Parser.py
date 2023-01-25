073aac2a445ac21f8af6dbef6bb90fcb945bd2aa
Jujumba <far77578@gmail.com>
2023-01-25 15:26:35 +0100

    Parser: Refactor

--- Parser.py
+++ Parser.py
@@ -1,30 +1,56 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import cv2
 from os import remove, getenv
 
 
 class Parser:
-    def get_screenshot(self, url: str, use_default_profile: bool = False):
-        options = webdriver.ChromeOptions()
+    def __init__(self):
+        self.driver = None
+        self.options = None
+        self.__deleted_header__ = False
+
+    def get_screenshot(self, url: str, use_default_profile: bool = False, pop_ups: list[str] = None, by: By = By.XPATH):
+        self.options = webdriver.ChromeOptions()
 
         if use_default_profile:
-            options.add_argument(rf"user-data-dir=C:\Users\{getenv('username')}\AppData\Local\Google\Chrome\User Data\Default")
+            self.options.add_argument(rf"user-data-dir=C:\Users\{getenv('username')}\AppData\Local\Google\Chrome\User Data\Default")
             
-        driver = webdriver.Chrome(chrome_options=options)
-        driver.get(url)
-        driver.maximize_window()
-        current = 0
-        height = driver.execute_script('return document.body.clientHeight')
-        step = driver.execute_script("return screen.height") - 160  # Magic number
+        self.driver = webdriver.Chrome(chrome_options=self.options)
+        self.driver.get(url)
+        self.driver.maximize_window()
 
+        self.accept_pop_ups(pop_ups, by)
+
+        self.scroll_and_save()
+
+    def accept_pop_ups(self, pop_ups, by):
+        if pop_ups is None:
+            return
+        for pop_up in pop_ups:
+            self.driver.find_element(by, pop_up).click()
+
+    def scroll_and_save(self):
+        current = 0
+        height = self.driver.execute_script('return document.body.clientHeight')
+        step = self.driver.execute_script("return screen.height") - 160  # Magic number
         images = []
         while current <= height:
             img_name = f"{str(current)}.png"
-            driver.save_screenshot(img_name)
+            self.driver.save_screenshot(img_name)
             images.append(cv2.imread(img_name))
+
             remove(img_name)
-            driver.execute_script(f"window.scrollBy(0, {step})")
+            self.driver.execute_script(f"window.scrollBy(0, {step})")
+
+            if not self.__deleted_header__:  # Removing header
+                self.__delete_header__()
+
             current += step
-        driver.quit()
+        self.driver.quit()
         im_v = cv2.vconcat(images)
-        cv2.imwrite('out.png', im_v)
+        cv2.imwrite('python.org.png', im_v)
+
+    def __delete_header__(self):
+        self.driver.execute_script("const header = document.getElementsByTagName(\"header\")[0]; header.remove()")
+        self.__deleted_header__ = True
