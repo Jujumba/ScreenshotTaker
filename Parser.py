203418d8d131e16ec46583285a77f69b4ca6c3a4
Jujumba <far77578@gmail.com>
2023-01-25 20:36:02 +0100

    Parser: Add naming for output picture

--- Parser.py
+++ Parser.py
@@ -3,27 +3,34 @@ from selenium.webdriver.common.by import By
 import cv2
 from os import remove, getenv
 
-
 class Parser:
     def __init__(self):
         self.__driver__ = None
         self.__options__ = None
         self.__deleted_header__ = False
         self.__images__ = []
+        self.__banned_chars__ = ['/','\\',':','*','?','"','<','>','|']
 
     def get_screenshot(self, url: str, use_default_profile: bool = False, pop_ups: list[str] = None, by: By = By.XPATH, delete_header: bool = False):
         self.__options__ = webdriver.ChromeOptions()
 
         if use_default_profile:
             self.__options__.add_argument(rf"user-data-dir=C:\Users\{getenv('username')}\AppData\Local\Google\Chrome\User Data\Default")
-            
+
         self.__driver__ = webdriver.Chrome(chrome_options=self.__options__)
         self.__driver__.get(url)
         self.__driver__.maximize_window()
 
         self.accept_pop_ups(pop_ups, by)
 
-        self.scroll_and_save(delete_header)
+        self.scroll(delete_header)
+        self.__driver__.quit()
+
+        img_name = url
+        for banned_char in self.__banned_chars__:
+            img_name = img_name.replace(banned_char, " ")
+
+        self.save(img_name)
 
     def accept_pop_ups(self, pop_ups, by):
         if pop_ups is None:
@@ -31,7 +38,7 @@ class Parser:
         for pop_up in pop_ups:
             self.__driver__.find_element(by, pop_up).click()
 
-    def scroll_and_save(self, delete_header):
+    def scroll(self, delete_header):
         current = 0
         height = self.__driver__.execute_script('return document.body.clientHeight')
         step = self.__driver__.execute_script("return screen.height") - 160  # Magic number
@@ -47,9 +54,14 @@ class Parser:
                 self.__delete_header__()
 
             current += step
-        self.__driver__.quit()
+
+
+    def save(self, output_name: str):
+        if not output_name.endswith(".png"):
+            output_name += ".png"
         im_v = cv2.vconcat(self.__images__)
-        cv2.imwrite('python.org.png', im_v)
+        cv2.imwrite(output_name, im_v)
+        self.__images__.clear()
 
     def __delete_header__(self):
         self.__driver__.execute_script("const header = document.getElementsByTagName(\"header\")[0]; header.remove()")
